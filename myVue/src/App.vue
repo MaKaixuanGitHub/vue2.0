f4698d5d18ec5b89a3e379dd97bebeaa0dbc5947
MaKaixuan <lanqiuxiaotong@outlook.com>
2022-03-15 22:57:40 +0800

    create first vue by vue-cli

---
+++ myVue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <b style="color: red; font-size: 50px;">这是马凯旋的VUE项目</b>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
